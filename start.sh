56310db3ec597712100b92c4b477457d7e209b1c
Lucas Zawacki <lfzawacki@gmail.com>
2013-01-02 21:20:33 -0200

    Correct start.sh to use chuck binary

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ guitar_synth="synths/guitar_synth.ck synths/sub_synth.ck synths/stk_synth.ck $mi
 guitar="$guitar_synth noise_pick.ck guitar.ck legato_guitar.ck picked_guitar.ck"
 input_handler="handlers/input_handler.ck handlers/slider_guitar_handler.ck handlers/guitar_handler.ck handlers/keyboard_handler.ck handlers/mouse_handler.ck"
 
-chuck-jack $guitar $input_handler main.ck:$1:$2:$3
+${CHUCK-chuck} $guitar $input_handler main.ck:$1:$2:$3
